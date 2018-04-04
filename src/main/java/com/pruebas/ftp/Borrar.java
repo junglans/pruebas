44a5bd339c35f4db7229cedab027daa2b6b1bf67
jf.jimenez <jf.jimenez@mnemo.es>
2018-04-04 15:50:02 +0200

    Mas modificaciones en Borrar.java

--- src/main/java/com/pruebas/ftp/Borrar.java
+++ src/main/java/com/pruebas/ftp/Borrar.java
@@ -2,8 +2,16 @@ package com.pruebas.ftp;
 
 public class Borrar {
 
+	private String attr1;
 	/**
 	 * Se añade el constructor por defecto.
 	 */
 	public Borrar() {}
+	public String getAttr1() {
+		return attr1;
+	}
+	public void setAttr1(String attr1) {
+		this.attr1 = attr1;
+	}
+	
 }

4c4363a420a57dde709c625eacb808fdc2799759
jf.jimenez <jf.jimenez@mnemo.es>
2018-04-05 10:39:29 +0200

    Se crean nuevos atributos: attr2,attr3.

--- src/main/java/com/pruebas/ftp/Prueba.java
+++ src/main/java/com/pruebas/ftp/Prueba.java
@@ -3,6 +3,14 @@ package com.pruebas.ftp;
 public class Prueba {
 
 	private String attr1;
+	private String attr2;
+	private String attr3;
+	public String getAttr3() {
+		return attr3;
+	}
+	public void setAttr3(String attr3) {
+		this.attr3 = attr3;
+	}
 	public Prueba(){}
 	public String getAttr1() {
 		return attr1;
@@ -10,5 +18,12 @@ public class Prueba {
 	public void setAttr1(String attr1) {
 		this.attr1 = attr1;
 	}
+	public String getAttr2() {
+		return attr2;
+	}
+	public void setAttr2(String attr2) {
+		this.attr2 = attr2;
+	}
+	
 	
 }

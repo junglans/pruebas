65c648d18c07106f130b3622bff9a8fb3148721b
jf.jimenez <jf.jimenez@mnemo.es>
2018-04-05 10:48:17 +0200

    Se agregan dos nuevos atributos

--- src/main/java/com/pruebas/ftp/Prueba.java
+++ src/main/java/com/pruebas/ftp/Prueba.java
@@ -5,12 +5,8 @@ public class Prueba {
 	private String attr1;
 	private String attr2;
 	private String attr3;
-	public String getAttr3() {
-		return attr3;
-	}
-	public void setAttr3(String attr3) {
-		this.attr3 = attr3;
-	}
+	private String attr4;
+	private String attr5;
 	public Prueba(){}
 	public String getAttr1() {
 		return attr1;
@@ -24,6 +20,23 @@ public class Prueba {
 	public void setAttr2(String attr2) {
 		this.attr2 = attr2;
 	}
-	
+	public String getAttr3() {
+		return attr3;
+	}
+	public void setAttr3(String attr3) {
+		this.attr3 = attr3;
+	}
+	public String getAttr4() {
+		return attr4;
+	}
+	public void setAttr4(String attr4) {
+		this.attr4 = attr4;
+	}
+	public String getAttr5() {
+		return attr5;
+	}
+	public void setAttr5(String attr5) {
+		this.attr5 = attr5;
+	}
 	
 }
